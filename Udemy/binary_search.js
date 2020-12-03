c1938b192decd8f08ba6caa76137a2f65ce93824
Zach McWhirter <zmac7811@gmail.com>
2020-12-03 11:50:30 -0600

    refactored binary search

--- Udemy/binary_search.js
+++ Udemy/binary_search.js
@@ -3,31 +3,34 @@ function binarySearch(arr, elem) {
     var start = 0;
     var end = arr.length - 1;
     var middle = Math.floor((start + end) / 2);
-    while(arr[middle] !== elem && start <= end) {
-        if(elem < arr[middle]){
+    while (arr[middle] !== elem && start <= end) {
+        if (elem < arr[middle]) {
             end = middle - 1;
         } else {
             start = middle + 1;
         }
         middle = Math.floor((start + end) / 2);
     }
-    if(arr[middle] === elem){
+    if (arr[middle] === elem) {
         return middle;
     }
     return -1;
 }
 
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 6))
+
 // Refactored Version
-function binarySearch(arr, elem) {
+function binarySearch2(arr, elem) {
     var start = 0;
     var end = arr.length - 1;
     var middle = Math.floor((start + end) / 2);
-    while(arr[middle] !== elem && start <= end) {
-        if(elem < arr[middle]) end = middle - 1;
+    while (arr[middle] !== elem && start <= end) {
+        if (elem < arr[middle]) end = middle - 1;
         else start = middle + 1;
         middle = Math.floor((start + end) / 2);
     }
     return arr[middle] === elem ? middle : -1;
 }
 
-binarySearch([2,5,6,9,13,15,28,30], 103)
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103))
+
